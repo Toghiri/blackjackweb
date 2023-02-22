17697a7657d6e56b0de4772eaeb04c1396ced2bc
Toghiri <pietrovaccariinfo@gmail.com>
2023-02-22 19:06:12 +0100

    feat: added cards

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TestView from '../views/TestView.vue'
+import HandView from '../views/HandView.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/test',
     name: 'test',
     component: TestView
+  },
+  {
+    path: '/hand',
+    name: 'hand',
+    component: HandView
   },
   {
     path: '/about',
---
+++ client/src/views/HandView.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container>
+    <v-raw>YOUR HAND:</v-raw>
+    <v-raw>
+        <v-col v-for="card in hand" :key="card">          
+            <v-img
+            :src="getCardImage(card)"
+            />
+        </v-col>
+    </v-raw>
+  </v-container>
+</template>
+
+<script>
+
+  export default {
+    name: 'HandView',
+    data:() => ( 
+    {
+        hand: ["XS", "KH", "AD", "QC"]
+    }),
+    methods: 
+    {
+        getCardImage: function(code)
+        {
+            let s = code[1]
+            let n = code[0]
+            return `./assets/cards/${this.getPrefix(n)}_of_${this.getSuffix(s)}.svg`
+            
+        },
+
+        getPrefix: function(n)
+        {
+            if (n == 'X') return '10'
+            if (n == 'J') return 'jack'
+            if (n == 'Q') return 'queen'
+            if (n == 'A') return 'ace'
+            if (n == 'K') return 'king'
+            return n
+        },
+
+        getSuffix: function(n)
+        {
+            if (n == 'S') return 'spades'
+            if (n == 'H') return 'hearts'
+            if (n == 'D') return 'diamonds'
+            if (n == 'C') return 'clubs'
+        },
+
+    }
+
+  }
+</script>
